b131cb9f0bee399d0f589849388927ed7eb6b2e8
Brijesh Chauhan <brijesh18chauhan@gmail.com>
2020-06-02 19:11:32 +0530

    commit--

--- Test.R
+++ Test.R
@@ -1,6 +1,9 @@
+<<<<<<< HEAD
 
 library(XML)
 fileurl <- "https://www.w3schools.com/xml/simple.xml"
 doc <- xmlTreeParse(fileurl,useInternal=TRUE)
 rootNode <- xmlRoot(doc)
 xmlName(rootNode)
+=======
+>>>>>>> b08bf19a776cbf61c4308629a3b809873d0d9b05
