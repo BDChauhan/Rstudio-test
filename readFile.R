c0c68d9bf9c468008d8ffe551e3f27425c99d8d0
Brijesh Chauhan <brijesh18chauhan@gmail.com>
2020-06-02 20:23:13 +0530

    Update readFile.R

--- readFile.R
+++ readFile.R
@@ -1,8 +1,10 @@
+##csv file read
 doc <- read.csv("./data/sample.csv")
 head(doc)
 
 ##simple json file read
 library(jsonlite)
+## fromJSON stores data in jsonData variable we can say
 jsonData <- fromJSON("https://www.learningcontainer.com/bfd_download/sample-json-file-2/")
 names(jsonData)
 
