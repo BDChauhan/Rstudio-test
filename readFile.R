2b438683adf14437d050d9c862ce26e3a7224444
Brijesh Chauhan <brijesh18chauhan@gmail.com>
2020-06-02 20:21:40 +0530

    read data from files of type csv and json

--- readFile.R
+++ readFile.R
@@ -1,2 +1,8 @@
 doc <- read.csv("./data/sample.csv")
 head(doc)
+
+##simple json file read
+library(jsonlite)
+jsonData <- fromJSON("https://www.learningcontainer.com/bfd_download/sample-json-file-2/")
+names(jsonData)
+
